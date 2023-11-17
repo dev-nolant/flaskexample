8eb34083d0c8cd563d714f0173a0e361ec9a59d7
Nolan Taft <killer123yo@gmail.com>
2023-11-16 23:27:34 -0500

    Update flaskAPI.py

--- flaskAPI.py
+++ flaskAPI.py
@@ -1,17 +1,26 @@
 from flask import Flask, Response
 import json
+import os
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def display_json():
     try:
-        with open('combined_players_data.json', 'r', encoding='utf-8') as json_file:
+        # Get the current date in the format "month/day/year"
+        current_date = datetime.now().strftime("%m/%d/%Y")
+
+        # Construct the JSON file path with the date
+        json_filename = f"data/combined_players_data_{current_date}.json"
+
+        if not os.path.exists(json_filename):
+            return "JSON file not found.", 404
+
+        with open(json_filename, 'r', encoding='utf-8') as json_file:
             data = json.load(json_file)
             pretty_json = json.dumps(data, indent=4)  # Pretty print the JSON
             return Response(pretty_json, content_type='application/json')
-    except FileNotFoundError:
-        return "JSON file not found.", 404
     except json.JSONDecodeError:
         return "Error decoding JSON.", 400
 
